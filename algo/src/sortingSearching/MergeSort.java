eeb4ecca6ad97d4b4ac264c50a518ce1bfe436fb
nishantk12 <nishantk12@gmail.com>
2015-07-30 21:18:19 -0700

    rev1

--- algo/src/sortingSearching/MergeSort.java
+++ algo/src/sortingSearching/MergeSort.java
@@ -1,23 +1,62 @@
 package sortingSearching;
 
+import java.util.Arrays;
+
 public class MergeSort {
+	static int numsG[] = {4,0,6,2,8,5,4};
 	public static void main(String[] args) {
-		int nums[] = {3,7,1,4,7,9,2,3,0};
-		mergeSort(nums,0,nums.length-1);
+		
+		int numstemp[] = numsG;
+		mergeSort(numstemp,0,numsG.length-1);
+		System.out.println(Arrays.toString(numsG));
 	}
 
 	private static void mergeSort(int[] nums, int start, int end) {
+		// TODO Auto-generated method stub
 		if(start<end){
-			int center = (end - start)/2;
+			int center = (start + end)/2;
 			mergeSort(nums,start,center);
-			mergeSort(nums,center,start);
+			mergeSort(nums,center+1,end);
 			merge(nums,start,center,end);
 		}
+		
+		
 	}
 
 	private static void merge(int[] nums, int start, int center, int end) {
+		System.out.println("start " + start + " center " + center +  " end " + end);
+		System.out.println(Arrays.toString(nums));
+		int oneTemp = start;
+		int twoTemp = center+1;
+		int pointer = start;
+		int count = 0;
+		int[] numstemp = nums;
 		
+		while(count<=(end-start)+1){
+			if(oneTemp<=center && twoTemp <= end){
+				if(nums[oneTemp] <= nums[twoTemp]){
+					numstemp[pointer] = nums[oneTemp];
+					oneTemp++;
+				}else{
+					numstemp[pointer] = nums[twoTemp];
+					twoTemp++;
+				}
+			}
+			
+			else if(twoTemp <= end){
+				numstemp[pointer] = nums[twoTemp];
+				twoTemp++;
+			}
+			
+			else if(oneTemp <= center){
+				numstemp[pointer] = nums[oneTemp];
+				oneTemp++;
+			}
+			
+			pointer++;
+			count++;
+		}
 		
+		numsG = numstemp;
 	}
-
 }

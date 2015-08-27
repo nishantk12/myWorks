6df37b85eab86e87b781ad5be0f75c18f13f8545
Nishant Kumar <nishantk12@gmail.com>
2015-08-27 13:37:16 -0700

    new package name

---
+++ algo/src/leetCode/ClimbStairs.java
@@ -0,0 +1,31 @@
+package leetCode;
+
+public class ClimbStairs {
+	static int[] memo = null;
+	public static void main(String[] args) {
+		int numberOfstairs = 44;
+		memo = new int[numberOfstairs+1];
+		//initializeMemo();    //Commenting this line, made this solution to be 99 %ile from 67%ile
+		int numberOfWays = findNumberOfWays(numberOfstairs);
+		System.out.println(numberOfWays);
+	}
+
+	private static void initializeMemo() {
+		for(int i=0;i<memo.length;i++){
+			memo[i] = -1;
+		}
+	}
+
+	private static int findNumberOfWays(int numberOfstairs) {
+		if(memo[numberOfstairs] == 0){
+			if(numberOfstairs <= 1)
+				memo[numberOfstairs] = 1;
+			else
+				memo[numberOfstairs] = findNumberOfWays(numberOfstairs -1) + findNumberOfWays(numberOfstairs -2);
+		}
+		
+		return memo[numberOfstairs];
+	}
+	
+	
+}

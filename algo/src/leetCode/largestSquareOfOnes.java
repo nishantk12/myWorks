63991e28de800c9afa6321e677cf710e0e9515d2
nkumar34 <nkumar34@EN4113962214.fulton.ad.asu.edu>
2015-08-30 19:12:55 -0700

    Largest square, working

--- algo/src/leetCode/largestSquareOfOnes.java
+++ algo/src/leetCode/largestSquareOfOnes.java
@@ -1,17 +1,30 @@
 package leetCode;
 
 public class largestSquareOfOnes {
+	static int[][] memo = null;
 	public static void main(String[] args) {
-		char[][] input = {{1,0,0,0},{0,1,1,1},{0,1,1,1},{0,1,1,1}};//{{0,1,1},{0,1,1},{0,0,0}};//{{0,1,1,0},{0,1,1,0},{0,0,0,0},{0,0,0,0}};// 
+		char[][] input = {{}};//{{0,1,1},{0,1,1},{0,0,0}};//{{0,1,1,0},{0,1,1,0},{0,0,0,0},{0,0,0,0}};//
+		if(input == null || input.length == 0){
+            System.out.println(0);
+        	return;
+		}
+		
+		if(input.length > 0 && input[0].length == 0){
+            System.out.println(0);
+        	return;
+		}
+		
+		memo = new int[input.length][input[0].length];
 		int largestSize = findBiggestSquare(input,0,0,0);
 		System.out.println(largestSize);
 	}
 
 	private static int findBiggestSquare(char[][] input, int side, int i, int j) {
-		if(i > input.length || j > input.length)
+		
+		if(i > input.length || j > input[0].length)
 			return side;
 		
-		if(i + side >= input.length || j + side >= input.length)
+		if(i + side >= input.length || j + side >= input[0].length)
 			return side;
 		
 		int count = 0;
@@ -28,13 +41,14 @@ public class largestSquareOfOnes {
 		
 		count--;
 		//System.out.println(count);
-		if(count == ((2*side) +1))
+		if(count == ((2*side) +1)){
+			
 			return Math.max(findBiggestSquare(input,side+1,i,j),Math.max(findBiggestSquare(input,side,i+1,j),
 					Math.max(findBiggestSquare(input,side,i,j+1),findBiggestSquare(input,side,i+1,j+1))));
-		else
+		}else{
 			return Math.max(findBiggestSquare(input,side,i+1,j),
 					Math.max(findBiggestSquare(input,side,i,j+1),findBiggestSquare(input,side,i+1,j+1)));
-		
+		}
 	}
 
 }

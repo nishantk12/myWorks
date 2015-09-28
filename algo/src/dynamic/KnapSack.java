62376edb8b09f08dae76826b2848fac7d173713c
Nishant Kumar <nishantk12@gmail.com>
2015-09-28 08:22:37 -0700

    Knapsack done. Learning : learn first, implement later

--- algo/src/dynamic/KnapSack.java
+++ algo/src/dynamic/KnapSack.java
@@ -2,9 +2,9 @@ package dynamic;
 
 public class KnapSack {
 	public static void main(String[] args) {
-		int[] objects = {1,2,3};
-		int[] values = {10,2,2};
-		int capacity = 4;
+		int[] objects = {2,3,8,10};
+		int[] values = {1,6,2,30};
+		int capacity = 11;
 		System.out.println(findMinCount(objects,values,capacity));
 	}
 
@@ -12,13 +12,17 @@ public class KnapSack {
 		if(capacity == 0) return 0;
 		//int count = 0;
 		int[][] dp = new int[objects.length+1][capacity+1];
-		for(int i=0;i<=objects.length;i++){
-			for(int j=0;j<=capacity;j++){
-				
+		for(int i=1;i<=objects.length;i++){
+			for(int j=1;j<=capacity;j++){
+				if(j >= objects[i-1]){
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - objects[i-1]] + values[i-1]);
+				}else{
+					dp[i][j] = dp[i-1][j];
+				}
+				System.out.println("["+i+"]"+"["+j+"]"+";"+dp[i][j]);
 			}
 		}
 		return dp[objects.length][capacity];
-		
 	}
 	
 }
